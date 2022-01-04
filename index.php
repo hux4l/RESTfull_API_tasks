75f09ad1a169b0ca30599c7e9810c5081a07c89a
tobas <tomas.baska@outlook.com>
2022-01-04 13:14:24 +0100

    headers

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 
+// init curl
 $ch = curl_init();
+
+// response headers array
+$response_headers = [];
+
+// callback function for response headers
+$header_callback = function($ch, $header) use (&$response_headers) {
+    $len = strlen($header);
+
+    // split head in two parts by :
+    $parts = explode(":", $header, 2);
+
+    // if is less than 2 return header
+    if (count($parts) < 2) {
+        return $len;
+    }
+
+    // header key will be header name, and value will be the header value
+    $response_headers[$parts[0]] = trim($parts[1]);
+
+    return $len;
+};
 
 $headers = [
     "Authorization: Client-ID fds56f4sdgdfg4dfg4dfg"
@@ -16,7 +38,9 @@ curl_setopt_array($ch, [
     // include our headers
     CURLOPT_HTTPHEADER => $headers,
     // get response headers
-    CURLOPT_HEADER => true
+    //CURLOPT_HEADER => true
+    // header function to get headers
+    CURLOPT_HEADERFUNCTION => $header_callback
 ]);
 
 // store response to variable
@@ -32,5 +56,8 @@ curl_close($ch);
 
 echo $status_code , "\n";
 echo $content_type, "\n";
+
+print_r($response_headers);
+
 echo $response , "\n";
 ?>
