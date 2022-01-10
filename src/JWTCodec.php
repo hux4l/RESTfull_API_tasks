ec6b960ad69d5e50ac5a6c4921405de4c0306b15
tobas <tomas.baska@outlook.com>
2022-01-10 14:57:17 +0100

    JWT generation

--- src/JWTCodec.php
+++ src/JWTCodec.php
@@ -2,6 +2,13 @@
 
 class JWTCodec
 {
+
+    // add encryption key as an argument so we can easily change it
+    public function __construct(private string $key)
+    {
+        
+    }
+
     // function to encode jwt
     public function encode(array $payload): string
     {
@@ -19,13 +26,39 @@ class JWTCodec
 
         $signature = hash_hmac("sha256",
                                                 $header . "." . $payload,
-                                                "2D4B6150645367566B58703273357638792F423F4528482B4D6251655468576D",
+                                                $this->key,
                                                 true);
 
         $signature =  $this->base64urlEncode($signature);
 
         return $header . "." . $payload . "." . $signature;
 
+    }
+
+    // decode, regular expression if matches return 1 else null
+    public function decode(string $token): array
+    {
+        if (preg_match("/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)/", $token, $matches) !== 1) {
+            throw new InvalidArgumentException("invalid token format");
+        }
+
+        // decode
+        $signature = hash_hmac("sha256",
+                                                $matches["header"] . "." . $matches["payload"],
+                                                $this->key,
+                                                true);
+
+        $signature_from_token = $this->base64urlDecode($matches["signature"]);
+
+        // if tokens do not match
+        if (!hash_equals($signature, $signature_from_token)) {
+            throw new Exception("Signature doesn't match");
+        }
+
+        // if oka decode token
+        $payload = json_decode($this->base64urlDecode($matches["payload"]), true);
+
+        return $payload;
     }
 
     // encode url
@@ -36,6 +69,11 @@ class JWTCodec
             ["-", "_", ""],
             base64_encode($text)
         );
+    }
+
+    private function base64urlDecode(string $text): string
+    {
+        return base64_decode(str_replace(["-", "_"], ["+", "/"], $text));
     }
 
     // generated encryption key 2D4B6150645367566B58703273357638792F423F4528482B4D6251655468576D
